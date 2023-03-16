3237af68b9135fe11efacb06adf590ffc9031044
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-03-16 16:02:22 -0500

    working js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+
+const galleryArray = galleryItems.map(({preview,description}) => {
+    const galleryItem = document.createElement('li');
+    galleryItem.classList.add('gallery__item');
+    galleryItem.insertAdjacentHTML('beforeend', `<img src="${preview}" class="gallery__image" alt="${description}">`);
+    return galleryItem;
+  });
+  
+  gallery.append(...galleryArray);
+
 console.log(galleryItems);
