880e22f3c787c3e67a35c1809e6227ff1d742004
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-03-17 18:13:09 -0500

    finally first gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
 
-const galleryArray = galleryItems.map(({preview,description}) => {
-    const galleryItem = document.createElement('li');
-    galleryItem.classList.add('gallery__item');
-    galleryItem.insertAdjacentHTML('beforeend', `<img src="${preview}" class="gallery__image" alt="${description}">`);
-    return galleryItem;
-  });
-  
-  gallery.append(...galleryArray);
+function createGallery(items) {
+  return items
+    .map((item) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+      </a>
+    </li>`)
+    .join("");
 
-console.log(galleryItems);
+}
+const addGallery = createGallery(galleryItems);
+
+gallery.innerHTML = addGallery;
+
+gallery.addEventListener ('click', clickOnImage);
+
+function clickOnImage(action) {
+
+  Action(action);
+
+  if (action.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+      <img src="${action.target.dataset.source}" width="800" height="600">
+  `);
+  instance.show();
+
+  gallery.addEventListener("keydown", (action) => {
+    if (action.code === "Escape") {
+      instance.close();
+    }
+  })
+
+}
+
+function Action(action) {
+  action.preventDefault();
+}
+
